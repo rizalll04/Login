9e8a0dce016fccb959f11c47e35f2f4f9797eb57
rizalll04 <nuhrizal914@gmail.com>
2026-08-11 15:19:19 +0700

    Update

--- src/main/java/com/example/login/config/LocalDataSourceConfig.java
+++ src/main/java/com/example/login/config/LocalDataSourceConfig.java
@@ -1,9 +1,12 @@
 package com.example.login.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+        import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,9 +18,9 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaRepositories(
-        basePackages = "com.example.login.repository",
-        entityManagerFactoryRef = "entityManagerFactory",
-        transactionManagerRef = "transactionManager"
+    basePackages = "com.example.login.repository",
+    entityManagerFactoryRef = "entityManagerFactory",
+            transactionManagerRef = "transactionManager"
 )
 public class LocalDataSourceConfig {
 
@@ -31,23 +34,56 @@ public class LocalDataSourceConfig {
     @Bean(name = "dataSource")
     @Primary
     public DataSource localDataSource() {
-        return localDataSourceProperties().initializeDataSourceBuilder().build();
+        return localDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
     }
 
     @Bean(name = "entityManagerFactory")
-    @Primary
-    public LocalContainerEntityManagerFactoryBean localEntityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(localDataSource());
-        factoryBean.setPackagesToScan("com.example.login.entity");
-        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        factoryBean.setPersistenceUnitName("local");
-        return factoryBean;
-    }
+@Primary
+public LocalContainerEntityManagerFactoryBean localEntityManagerFactory() {
 
+    LocalContainerEntityManagerFactoryBean factoryBean =
+            new LocalContainerEntityManagerFactoryBean();
+
+    factoryBean.setDataSource(localDataSource());
+
+    factoryBean.setPackagesToScan(
+            "com.example.login.entity"
+    );
+
+    factoryBean.setJpaVendorAdapter(
+            new HibernateJpaVendorAdapter()
+    );
+
+    factoryBean.setPersistenceUnitName("local");
+
+    Map<String, Object> properties = new HashMap<>();
+
+    properties.put(
+            "hibernate.dialect",
+            "org.hibernate.dialect.Oracle10gDialect"
+    );
+
+    properties.put(
+            "hibernate.hbm2ddl.auto",
+            "none"
+    );
+
+    properties.put(
+            "hibernate.show_sql",
+            true
+    );
+
+    factoryBean.setJpaPropertyMap(properties);
+
+    return factoryBean;
+}
     @Bean(name = "transactionManager")
     @Primary
     public PlatformTransactionManager localTransactionManager() {
-        return new JpaTransactionManager(localEntityManagerFactory().getObject());
+        return new JpaTransactionManager(
+                localEntityManagerFactory().getObject()
+        );
     }
 }
